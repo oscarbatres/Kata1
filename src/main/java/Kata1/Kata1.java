265f1623dff7993cc7c5e391ca652ab77ff3132e
Oscar <obatres97@gmail.com>
2020-10-07 10:09:19 +0100

    First version of Kata1

---
+++ src/main/java/Kata1/Kata1.java
@@ -0,0 +1,13 @@
+package Kata1;
+
+import java.util.Date;
+
+public class Kata1 {
+
+    public static void main(String[] args) {
+        
+        Person person = new Person("Oscar", new Date(97,0,10));
+        
+    }
+    
+}
